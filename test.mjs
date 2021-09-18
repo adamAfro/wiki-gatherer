cef147759de9159a1a8dd85770cde3965e16360d
adamAfro <adam.per.jakubczak@gmail.com>
2021-09-18 19:27:33 +0200

    outputing test's results in test-results-catalog

--- test.mjs
+++ test.mjs
@@ -5,3 +5,25 @@ import {
 let gatherer = new Gatherer("https://pl.wikipedia.org/w/api.php");
 let members = await gatherer.gather(3183659, Scanners.person);
 
+import {
+    writeFile as write,
+    mkdir as make,
+    rm as remove
+} from "fs/promises";
+
+
+let cataloging = remove("test-results", { recursive: true }).catch(error => {})
+    .then(function () { make("test-results") }).catch(error => {});
+
+let writing = cataloging.then(function() {
+
+    return loop(); function loop(i = 0) {
+
+        if (i < members.length)
+            return write(`test-results/${members[i].title}.json`, JSON.stringify(members[i]))
+                .then(() => loop(i + 1));
+        else
+            return;
+    };
+
+}).then(() => console.log(members, `(${members.length}) -> ./test-results`));

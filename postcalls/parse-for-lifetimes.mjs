b7bd930208d50f845be0ba8f801f8795f115eff6
adamAfro <adam.per.jakubczak@gmail.com>
2021-09-20 20:23:13 +0200

    showing results on a timetable

--- postcalls/parse-for-lifetimes.mjs
+++ postcalls/parse-for-lifetimes.mjs
@@ -1,3 +1,7 @@
+export const Meta = {
+    purpose: "getting information from API res. in order to visualize it on a timetable"
+};
+
 import {
     readFileSync as read,
     readdirSync as list,
@@ -6,28 +10,48 @@ import {
 
 let data = [], ignored = [];
 
+function totime(pseudodate) {
+
+    // making WikiData dates normal
+    if (pseudodate) {
+
+        if (pseudodate.slice(6,8) == "00")
+            pseudodate = pseudodate.slice(0,6) + "01" + pseudodate.slice(8);
+
+        if (pseudodate.slice(9,11) == "00")
+            pseudodate = pseudodate.slice(0,9) + "01" + pseudodate.slice(11);
+
+        if (pseudodate.startsWith('+'))
+            return pseudodate.slice(1, -1) + ".000Z";
+        else if (pseudodate.startsWith('-'))
+            return "-00" + pseudodate.slice(1, -1) + ".000Z";
+    }
+}
+
 for (let filename of list("test-results/download")) {
 
     let input = read(`test-results/download/${filename}`, { encoding: "utf-8" });
     let item = JSON.parse(input);
 
     let entity = {
-        title: item.title,
-        birth: item.data.claims.P569 ? item.data.claims.P569[0]?.mainsnak?.datavalue?.value?.time : undefined,
-        death: item.data.claims.P570 ? item.data.claims.P570[0]?.mainsnak?.datavalue?.value?.time : undefined
+        content: item.title,
+        start: item.data.claims.P569 ? totime(item.data.claims.P569[0]?.mainsnak?.datavalue?.value.time) : undefined,
+        end: item.data.claims.P570 ? totime(item.data.claims.P570[0]?.mainsnak?.datavalue?.value.time) : undefined
     };
 
-    if (entity.birth) {
+    if (entity.start) {
 
-        // TODO append +100Y death date if (!entity.date)
+        // extending lifetime
+        if(!entity.end || Math.abs(Date.parse(entity.end) - Date.parse(entity.start)) < 60000*60*24*365*13) // assuming adolescence
+            entity.end = new Date(Date.parse(entity.start) + 60000*60*24*365*64);
 
         data.push(entity);
 
     } else
-        ignored.push(entity.title);
+        ignored.push(entity.content);
 }
 
-console.log({ "!entity.birth": ignored });
+console.log({ "!entity.start": ignored });
 
 let output = JSON.stringify(data, null, '\t');
 
--- test.mjs
+++ test.mjs
@@ -2,7 +2,7 @@ import Gatherer from "./index.mjs";
 
 let gatherer = new Gatherer("https://pl.wikipedia.org/w/api.php");
 
-let members = await gatherer.gather(1949616);
+let members = await gatherer.gather(1810130);
 
 import {
     writeFile as write,
@@ -11,14 +11,15 @@ import {
 } from "fs/promises";
 
 let cataloging = remove("test-results", { recursive: true }).catch(error => {})
-    .then(function () { make("test-results") }).catch(error => {});
+    .then(() => make("test-results").catch(error => {}))
+    .then(() => make("test-results/download").catch(error => {}))
 
 let writing = cataloging.then(function() {
 
     return loop(); function loop(i = 0) {
 
         if (i < members.length)
-            return write(`test-results/${members[i].title}.json`, JSON.stringify(members[i]))
+            return write(`test-results/download/${members[i].title}.json`, JSON.stringify(members[i]))
                 .then(() => loop(i + 1));
         else
             return;

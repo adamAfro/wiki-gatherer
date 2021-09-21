fd59d8294aff97cb5ce697cd2984decab32b1bd7
adamAfro <adam.per.jakubczak@gmail.com>
2021-09-21 21:07:13 +0200

    dirs for testing
    dirpath is always the script one

--- test.mjs
+++ test.mjs
@@ -1,24 +1,35 @@
-import Gatherer from "./index.mjs";
-
-let gatherer = new Gatherer("https://pl.wikipedia.org/w/api.php");
-
-let members = await gatherer.gather("Kategoria: Polscy_filozofowie_XX_wieku");
-
 import {
     writeFile as write,
     mkdir as make,
     rm as remove
 } from "fs/promises";
 
-let cataloging = remove("test-results/download", { recursive: true }).catch(error => {})
-    .then(() => make("test-results/download").catch(error => {}))
+import Gatherer from "./index.mjs";
+
+
+import {
+    fileURLToPath as purl
+} from 'url';
+import * as Path from 'path';
+
+const dirpath = Path.dirname(purl(import.meta.url));
+
+
+let gatherer = new Gatherer("https://pl.wikipedia.org/w/api.php");
+
+let members = await gatherer.gather("Kategoria: Polscy_filozofowie_XX_wieku"), paths = {
+    workspace: `${dirpath}/test-results/download`
+};
+
+let cataloging = remove(paths.workspace, { recursive: true }).catch(error => {})
+    .then(() => make(paths.workspace).catch(error => {}))
 
 let writing = cataloging.then(function() {
 
     return loop(); function loop(i = 0) {
 
         if (i < members.length)
-            return write(`test-results/download/${members[i].title}.json`, JSON.stringify(members[i]))
+            return write(`${paths.workspace}/${members[i].title}.json`, JSON.stringify(members[i]))
                 .then(() => loop(i + 1));
         else
             return;
